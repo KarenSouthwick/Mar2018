4b89a26c5a703f66ace37d3614591cc44dcf56a9
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-03-28 15:03:52 +0100

    2b

--- March2018/Tests/SmokeTests/VerfiyLogIn.cs
+++ March2018/Tests/SmokeTests/VerfiyLogIn.cs
@@ -24,7 +24,7 @@ namespace March2018.Tests.SmokeTests
         [Test]
         public void OpenAppTest()
         {
-            driver.FindElement(By.Id("UserName")).SendKeys("alunjones");
+            driver.FindElement(By.Id("UserName")).SendKeys("mattyates");
             driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
             driver.FindElement(By.Id("do-submit")).Click();
 
