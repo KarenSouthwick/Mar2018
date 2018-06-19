7ceb097296477a7004859e91a3ef94bab21f4587
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-06-19 14:15:08 +0100

    added a further qa script

--- March2018/Tests/SpecBuilderDev2/ProdCatDelete.cs
+++ March2018/Tests/SpecBuilderDev2/ProdCatDelete.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 using System.Threading;
 
@@ -13,15 +13,16 @@ namespace March2018.Tests.SpecBuilderDev2
     [TestFixture]
     public class ProdCatDelete
     {
-        IWebDriver driver = new FirefoxDriver();
+        IWebDriver driver = new ChromeDriver();
 
         [OneTimeSetUp]
         public void Initialize()
         {
-            driver.Navigate().GoToUrl("https://dev-platform.authenticateis.com/Account/Logon");
+            driver.Navigate().GoToUrl("https://qa-platform.authenticateis.com/Account/Logon");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.FindElement(By.Id("UserName")).SendKeys("karensouthwick");
-            driver.FindElement(By.Id("Password")).SendKeys("Exchange!6");
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 974);
+            driver.FindElement(By.Id("UserName")).SendKeys("User1508");
+            driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
             driver.FindElement(By.Id("do-submit")).Click();
             driver.FindElement(By.Id("do-closePopup")).Click();
             Thread.Sleep(3000);
@@ -49,8 +50,8 @@ namespace March2018.Tests.SpecBuilderDev2
             driver.FindElement(By.Id("Category_Name")).SendKeys("Hungary");
             driver.FindElement(By.XPath("//button[@type='submit']")).Click();
             driver.FindElement(By.LinkText("add product")).Click();
-            driver.FindElement(By.Id("ProductLine_Name")).SendKeys("product6");
-            driver.FindElement(By.Id("ProductLine_ReferenceCode")).SendKeys("pp6");
+            driver.FindElement(By.Id("ProductLine_Name")).SendKeys("product7");
+            driver.FindElement(By.Id("ProductLine_ReferenceCode")).SendKeys("pp7");
             driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
             driver.FindElement(By.XPath("//div[@id='step-0']/div[3]/button")).Click();
             driver.FindElement(By.LinkText("Hungary")).Click();
