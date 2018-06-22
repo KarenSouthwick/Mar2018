52787187a35871370fbe21748e9f551e5bed8ce4
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-06-22 11:08:59 +0100

    updated prod mapping script

--- March2018/Tests/SmokeTests/VerifyMapProd.cs
+++ March2018/Tests/SmokeTests/VerifyMapProd.cs
@@ -8,6 +8,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace March2018.Tests.SmokeTests
 {
@@ -18,34 +19,34 @@ namespace March2018.Tests.SmokeTests
         [SetUp]
         public void Initialize()
         {
-            driver.Navigate().GoToUrl("https://uat-platform.authenticateis.com/Account/Logon");
+            driver.Navigate().GoToUrl("https://qa-platform.authenticateis.com/Account/Logon");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 974);
         }
 
         [Test]
         public void MapProdTest()
         {
-            driver.FindElement(By.Id("UserName")).SendKeys("helenmaxwell");
+            driver.FindElement(By.Id("UserName")).SendKeys("User2867");
             driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
             driver.FindElement(By.Id("do-submit")).Click();
+            driver.FindElement(By.Id("do-closePopup")).Click();
+            Thread.Sleep(3000);
 
             driver.FindElement(By.XPath("//li[@id='do-mapProductsCount']/a")).Click();
-            driver.FindElement(By.LinkText("map product")).Click();
+            driver.FindElement(By.LinkText("map supply chain")).Click();
+            Thread.Sleep(3000);
 
-            //to be completed as cannot select frame
             driver.SwitchTo().Frame("mfp-iframe");
-            driver.FindElement(By.Name("ProductLinkSearchRequests[0].Company.Name")).Clear();
-            driver.FindElement(By.Name("ProductLinkSearchRequests[0].Company.Name")).SendKeys("Aviagen");
-            driver.FindElement(By.Id("Aviagen")).Click();
+            IWebElement elem = driver.FindElement(By.XPath("//input[@name='ProductLinkSearchRequests[0].Company.Name']"));
+            elem.Clear();
+            elem.SendKeys("Bidfood");
+            elem.SendKeys(Keys.ArrowDown);
+            elem.SendKeys(Keys.Enter);
 
 
-
-            driver.FindElement(By.Name("ProductLinkSearchRequests[0].ProductName")).SendKeys("trev");
-
-            driver.FindElement(By.Name("ProductLinkSearchRequests[0].ProductReference")).SendKeys("123");
-
-
-
+            driver.FindElement(By.Name("ProductLinkSearchRequests[0].ProductName")).SendKeys("treva");
+            driver.FindElement(By.Name("ProductLinkSearchRequests[0].ProductReference")).SendKeys("1234");
 
             driver.FindElement(By.CssSelector("[class='button action']")).Click();
 
