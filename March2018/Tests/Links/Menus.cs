efb0eae55916f167a36575ef9f9015a99e366150
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-06-22 15:21:55 +0100

    Links folder

--- March2018/Tests/Links/Menus.cs
+++ March2018/Tests/Links/Menus.cs
@@ -8,6 +8,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
 using OpenQA.Selenium.Interactions;
+using System.Threading;
 
 namespace March2018.Tests.Links
 {
@@ -17,17 +18,22 @@ namespace March2018.Tests.Links
         public void Test()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://uat-platform.authenticateis.com/Account/Logon");
+            driver.Navigate().GoToUrl("https://qa-platform.authenticateis.com/Account/Logon");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 974);
 
-            driver.FindElement(By.Id("UserName")).SendKeys("angelgaltrey");
+            driver.FindElement(By.Id("UserName")).SendKeys("User2039");
             driver.FindElement(By.Id("Password")).SendKeys("Aramark22");
             driver.FindElement(By.Id("do-submit")).Click();
+            driver.FindElement(By.Id("do-submitPrimary")).Click();
+            driver.FindElement(By.Id("do-closePopup")).Click();
+            Thread.Sleep(3000);
 
             driver.FindElement(By.Id("productLineSearchQuery")).Clear();
-            driver.FindElement(By.Id("productLineSearchQuery")).SendKeys("Guacamole");
+            driver.FindElement(By.Id("productLineSearchQuery")).SendKeys("Brown Sauce 10kg");
             driver.FindElement(By.Name("action:ProductLineAdvancedSearch")).Click();
-            driver.FindElement(By.LinkText("Guacamole")).Click();
+            driver.FindElement(By.LinkText("Brown Sauce 10kg")).Click();
+            driver.FindElement(By.XPath("//div[@id='step-0']/div[3]/button")).Click();
                       
             IWebElement elem = driver.FindElement(By.LinkText("KPI's"));
             Actions builder = new Actions(driver);
